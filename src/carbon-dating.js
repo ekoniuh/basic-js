ee3074ab2c7157a2136e10b1ca4e043788540f0d
“ekoniuh” <ekoniuh@gmail.com>
2020-09-07 16:28:59 +0300

    feat: solution task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const num = parseFloat(sampleActivity);
+  if (
+    typeof sampleActivity !== "string" ||
+    !num ||
+    num <= 0 ||
+    num > MODERN_ACTIVITY
+  )
+    return false;
+
+  return Math.ceil(
+    Math.abs(Math.log(MODERN_ACTIVITY / num) / (0.693 / HALF_LIFE_PERIOD))
+  );
 };
+
+// module.exports = function dateSample(sampleActivity) {
+//   return typeof sampleActivity === "string" &&
+//     +sampleActivity < MODERN_ACTIVITY &&
+//     +sampleActivity > 0
+//     ? Math.ceil(
+//         Math.abs(Math.log(parseFloat(sampleActivity) / 15) / (0.693 / 5730))
+//       )
+//     : false;
+// };
+
+// function dateSample(sampleActivity) {
+//   if (
+//     typeof sampleActivity === "string" &&
+//     +sampleActivity < 15 &&
+//     +sampleActivity > 0
+//   ) {
+//     return Math.ceil(Math.abs(Math.log(+sampleActivity / 15) / (0.693 / 5730)));
+//   }
+//   return false;
+// };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const controlSequences = [
+    "--discard-next",
+    "--discard-prev",
+    "--double-next",
+    "--double-prev",
+  ];
+  if (!(arr instanceof Array)) throw new Error("Not an Array!");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "asd") {
+    }
+  }
 };
